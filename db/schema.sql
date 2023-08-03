d6246f50bb8cf897d16dcba4359f439a5c30dcc0
Engine <rg.dartigue@gmail.com>
2023-08-03 17:39:36 -0400

    Added schema to db folder with person_db database and with department and role tables. Still trying to figure out how to add the employee table, so it has been commented out for now. Server.js file created.

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS person_db;
+CREATE DATABASE person_db;
+
+USE person_db;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  depart_name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+);
+
+-- CREATE TABLE employee (
+--   id INT,
+--   first_name VARCHAR(30),
+--   last_name VARCHAR(30),
+--   role_id INT,
+--   manager_id INT,
+--   FOREIGN KEY (role_id)
+--   REFERENCES role(id)
+-- );
