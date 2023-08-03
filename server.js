d802215a1b1fa9bd2ee42f5a62cd44fc77cea1e7
Engine <rg.dartigue@gmail.com>
2023-08-03 17:52:19 -0400

    installed npm. Connected to person_db in server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// -----middleware--------------
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'person_db'
+    },
+    console.log(`Connected to the person_db`)
+);
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
